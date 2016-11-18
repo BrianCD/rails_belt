c64abc9ceab764c4e22aded56b9db725a1908e69
BrianCD <bkwinner2@gmail.com>
2016-11-18 14:05:51 -0600

    Fixed regression of lenders balances not decreasing

--- app/models/borrower.rb
+++ app/models/borrower.rb
@@ -7,7 +7,7 @@ class Borrower < ActiveRecord::Base
   validates :goal, numericality: { greater_than: 0 }
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :email, presence: true, format: { with: EMAIL_REGEX }
-  validates_each :email do |record, name, email|
+  validates_each :email, on: :create do |record, name, email|
     record.errors.add(name, "has already been taken") if Borrower.find_by_email(email.downcase) || Lender.find_by_email(email.downcase)
   end
   has_many :histories
